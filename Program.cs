547bed37cc2301b60070177e72c061022a9d92ed
Artem Chashchin <92800788+alexvilno@users.noreply.github.com>
2023-01-15 15:25:51 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,9 +2,41 @@ using System;
 
 class Program
 {
+    public static void Main1()
+    {
+        Random random = new Random();
+        double n = Convert.ToDouble(Console.ReadLine());
+        double k = 0; //количество благоприятных исходов
+        for (int i = 0; i < n; i++)
+        {
+            double alpha1 = random.NextDouble();
+            double alpha2 = random.NextDouble();
+            if (alpha1 <= 0.1)
+            {
+                alpha1 = 18;
+            }
+            else if (alpha1 > 0.1 && alpha1 <= 0.6)
+            {
+                alpha1 = 19;
+            }
+            else
+            {
+                alpha1 = 21;
+            }
+            alpha2 = 20 * Math.Pow(alpha2, 0.625);
+            double d = 0.4 * alpha1 + 0.6 * alpha2; //выражение d
+            if (d > 8.4)  //проверка условия задачи
+            {
+                k++;
+            }
+        }
+        double st_prob = k / n; //классическая вероятность
+        Console.WriteLine(st_prob);
+    }
+
     public static void Main()
     {
-        const double teor_ver = 0.97;
+        const double th_prob = 0.97;
         Random random = new Random();
         double n = Convert.ToDouble(Console.ReadLine());
         double counter = 0;
@@ -27,23 +59,23 @@ class Program
                 {
                     alpha1 = 21;
                 }
-                alpha2 = 20 * Math.Pow(alpha2, 1.6);
+                alpha2 = 20 * Math.Pow(alpha2, 0.625);
                 double d = 0.4 * alpha1 + 0.6 * alpha2;
                 if (d > 8.4)
                 {
                     k++;
                 }
             }
-            double veroyatnost = k / n;
-            Console.WriteLine(veroyatnost);
-            if (Math.Abs(veroyatnost - teor_ver) <= 0.05)
+            double prob = k / n;
+            //Console.WriteLine(veroyatnost);
+            if (Math.Abs(prob - th_prob) <= 0.05)
             {
                 counter++;
             }
         }
-        double veroyat3 = counter / n;
+        double prob_eps = counter / n; //вероятность отклонения
 
-        Console.WriteLine(veroyat3);
+        Console.WriteLine(prob_eps);
     }
 
 }
